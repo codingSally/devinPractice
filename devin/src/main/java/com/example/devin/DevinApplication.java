4b7375c1d9dfa5f2bebf96019d921ff683b83f3b
xueshangling <xueshangling.com>
2025-03-08 20:05:07 +0800

    init

--- devin/src/main/java/com/example/devin/DevinApplication.java
+++ devin/src/main/java/com/example/devin/DevinApplication.java
@@ -2,12 +2,14 @@ package com.example.devin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DevinApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevinApplication.class, args);
+		System.out.println("启动成功");
 	}
 
 }
