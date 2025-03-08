d9e8dd79e07fa65c5ec47458e087afc9ea4c62d8
xueshangling <xueshangling.com>
2025-03-08 16:28:35 +0800

    init

---
+++ devin/src/main/java/com/example/devin/DevinApplication.java
@@ -0,0 +1,13 @@
+package com.example.devin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevinApplication.class, args);
+	}
+
+}
